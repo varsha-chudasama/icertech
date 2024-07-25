a9bef3b9097d51707285e75322701f3bfaa2aff7
varsha-chudasama <147918626+varsha-chudasama@users.noreply.github.com>
2024-07-25 09:25:39 +0530

    header with responsive

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -20489,6 +20489,7 @@ var Header = /*#__PURE__*/function () {
     key: "init",
     value: function init() {
       this.HeaderHover();
+      this.ResponsiveHeader();
     }
   }, {
     key: "HeaderHover",
@@ -20514,6 +20515,52 @@ var Header = /*#__PURE__*/function () {
         }
         handleWindowResizeDeskSize();
         $(window).resize(handleWindowResizeDeskSize);
+      });
+    }
+  }, {
+    key: "ResponsiveHeader",
+    value: function ResponsiveHeader() {
+      $(document).ready(function () {
+        function handleWindowResizeMegaMenu() {
+          var windowWidth = $(window).width();
+          if (windowWidth >= 0 && windowWidth <= 992) {
+            $('.menu-toggle').click(function (e) {
+              e.preventDefault();
+              $('.burgar-menu').toggleClass('activate');
+              $('.header').toggleClass('header-active');
+              $('.header-btn-end ,.top-header , .header-logo').toggleClass('d-none');
+              $('html').toggleClass('overflow-hidden');
+              $('.header').removeClass('header-megamenu-active');
+              $('.menu-item').removeClass('res-menu-active');
+            });
+            $('.menu-item').appendTo('.menu-items');
+            $('.tab-data1').appendTo('.tab-item1');
+            $('.tab-data2').appendTo('.tab-item2');
+            $('.tab-data3').appendTo('.tab-item3');
+            $('.menu-item').each(function () {
+              if ($(this).children(".mega-menu").length === 0) {
+                return;
+              } else {
+                var menuItem = $(this);
+                menuItem.click(function (e) {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  if (menuItem.hasClass('res-menu-active')) {
+                    $('.header').removeClass('header-megamenu-active');
+                    menuItem.removeClass('res-menu-active');
+                  } else {
+                    $('.header').removeClass('header-megamenu-active');
+                    $('.res-menu-active').removeClass('res-menu-active');
+                    $('.header').addClass('header-megamenu-active');
+                    menuItem.addClass('res-menu-active');
+                  }
+                });
+              }
+            });
+          }
+        }
+        handleWindowResizeMegaMenu();
+        $(window).resize(handleWindowResizeMegaMenu);
       });
     }
   }]);
